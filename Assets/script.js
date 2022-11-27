c2909ee8e99b5d29e9ac3ce5dbe15e6549b02a15
swmcguire <swmcguire1@comcast.net>
2022-11-27 16:18:57 -0600

    Updated readme and made adjustments to HMTL and updated color portion of jscript

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,56 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
 
+  //-------------------------------Set current date and time to display---------//
   var today = dayjs().format('MMM-DD-YYYY: HH:mm:ss');
   $('#currentDay').text(today);
-
+  
+ //-------------------------------Set current time for comparison---------//
   var currentHour = dayjs().format('HH');
 
-  function getTime() {
+    
+  //-------------------------------Use ParseInt to get the hour from the class/ID------------//
+    $(".time-block").each(function() {
+      var hourWindow = parseInt($(this).attr("id").split("hour-")[1]);
 
-  //if current time is before time block then past 
-  //if current time is between time block then present 
-  //if current time is after current time block then future
+  //--------------------------------Console.log to check to see what is being shown---------//
+      console.log(hourWindow, currentHour);
+    
+  //------------------------------if current time is before hour window then past-----------//
+  if (hourWindow < currentHour) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+    
 
-
-
+  //------------------------------if current time is equal to hour window then present-----------//
+  } else if (hourWindow == currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+    $(this).removeClass("future");
+  
+  //------------------------------if current time is after hour window then future-----------//  
+  } else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
   }
 
-  console.log(currentHour);
+})
+  
+//--------------------------------Add OnClick Event-----------//  
+$(".saveBtn").on("click",function() {
+//--------------------------------When button clicked then save to local storage 
+var textArea = $('textarea');
+var hourWindow = 
+//localStorage.setItem(x,y)
+  console.log("button click");
+
+});
 
 
+//--------------------------------When page reloaded add the item back to the page--------//
+//localStorage.getItem();
 
 
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
 });
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Calendar-App
-Calendar App that will allow a user to save events for each hour of the day by modifying some starter code.
+
+## Description
+
+Being that work has been ramping up and its been more difficult to stay on schedule with the additional tasks it would be nice to have a daily calendar that I can update so I can stay on task all day.
+
+When I open the calendar App I will be able to see the current date and time.  I am then able to write updates within the different time blocks for what needs to get done during that timeframe.  When the time falls within the current time block the schedule will display a red tint, if the time block is in the past it will display a grey tint, and if the time block is in the future it will display green.
+
+Since the tasks will be stored in the local storage, I will be able to refresh the page and it will pull in the calendar detail when I refresh the page.
+
+This project was helpful to understand dayJS - I feel that was the easiest part of the assignment.  Getting the correct blocks to display the correct colors and saving to local storage proved to be a bit more difficult than I originally thought it would.
+
+## Installation
+
+No installation necessary - simply launch from the designated webpage and begin using.  You are able to type in the different text boxes and save the text to the local storage.  Once you refresh the page the text will return so you know what needs to be done for the day.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+https://www.w3schools.com/jsref/api_storage.asp
+
+JavaScript & JQuery:  Interactive front-end web development by Jon Duckett
+
+https://www.w3schools.com/jsref/event_onclick.asp 
+
+
+## License
+
+MIT License
+
+Copyright (c) 2022 swmcguire
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
