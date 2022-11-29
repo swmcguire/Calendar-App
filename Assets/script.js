e4b70051c202cdb6feb2b0b85c7b72dd0bdcfdef
swmcguire <swmcguire1@comcast.net>
2022-11-28 22:12:45 -0600

    final push then done

--- Assets/script.js
+++ Assets/script.js
@@ -38,8 +38,8 @@ $(function () {
 //--------------------------------Add OnClick Event-----------//  
 $(".saveBtn").on("click",function(event) {
 //--------------------------------When button clicked then save to local storage 
-var textArea = $(this).siblings(".description"); //Very close - need to get the value
-var hourWindow = $(this).parent(); // figure out how to get the time here 
+var textArea = $(this).siblings(".description").val(); //call the sibling of the saveBtn class (div/textarea) -- get the description text box
+var hourWindow = $(this).parent().attr("id"); // call the parent of the .saveBtn class (returns the div) --then get the id of "hour-__"
 localStorage.setItem(hourWindow,textArea);
   console.log("button click");
   console.log(textArea);
@@ -47,10 +47,19 @@ localStorage.setItem(hourWindow,textArea);
 console.log($(event.target));
 });
 
-
 //--------------------------------When page reloaded add the item back to the page--------//
 //localStorage.getItem();
 
+$("#hour-8 .description") .val(localStorage.getItem("hour-8"));
+$("#hour-9 .description") .val(localStorage.getItem("hour-9"));
+$("#hour-10 .description") .val(localStorage.getItem("hour-10"));
+$("#hour-11 .description") .val(localStorage.getItem("hour-11"));
+$("#hour-12 .description") .val(localStorage.getItem("hour-12"));
+$("#hour-13 .description") .val(localStorage.getItem("hour-13"));
+$("#hour-14 .description") .val(localStorage.getItem("hour-14"));
+$("#hour-15 .description") .val(localStorage.getItem("hour-15"));
+$("#hour-16 .description") .val(localStorage.getItem("hour-16"));
+$("#hour-17 .description") .val(localStorage.getItem("hour-17"));
 
 
 });
